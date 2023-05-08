86ea51a647a4359e5adf34bfc6faaf6ff6eeda5a
SamuraisEpic <SamuraisEpic@hotmail.com>
2023-05-08 14:21:00 -0400

    modified:   design.py 	modified:   main.py - window (still broken, needs fixing)

--- design.py
+++ design.py
@@ -1,9 +1,11 @@
 '''
-i have no idea how design is gonna work, but here we go
+i have no idea how design doc is gonna work, but here we go
 
-start with a plain qt window
+so the idea is to have a qt window for the user to interact with to provide files and whatnot.
+ideally it'd also have support for cli switches of you prefer a terminal, and the visualization would be in a seperate window, similar to qemu
+i dont know how i'd go about doing that yet, but sys is used for switch support in the event i do later on.
 
-add some buttons
+resource for a plain window is here: https://www.pythonguis.com/tutorials/creating-your-first-pyqt-window/
 
-i hope im doing this right?
+
 '''
--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 #Mitam Kacha
 #Music Visualizer
+
+#start by importing sys for cli switches, this would help if the user wants to interact through a terminal similar to qemu.
+#also relevant modules from pyqt5 (obviously)
+
+import sys
+from PyQt5 import QApplication, QWidget
+
+#make a qapplication called gui, since it's the gui frontend for the program
+
+gui = QApplication(sys.argv) #sys.argv allows for cli switches
+
+#qwidget acts as the windo
+window = QWidget()
+
+#have to show the window since all top level widgets are hidden
+window.show()
+
+#now start the event loop
+gui.exec()
+
