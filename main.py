83f0144a60d3b226cc5fa0b24030e11e096f5654
SamuraisEpic <69528792+SamuraisEpic@users.noreply.github.com>
2023-06-17 22:15:15 -0400

    modified:   main.py: remove pyqt junk

--- main.py
+++ main.py
@@ -1,25 +1,9 @@
-#Mitam Kacha
+#SamuraisEpic
 #Music Visualizer
 
 #start by importing sys for cli switches, this would help if the user wants to interact through a terminal similar to qemu.
 #also relevant modules from pyqt5 (obviously)
-'''
-import sys
-from PyQt5 import QApplications,QWidget
 
-#make a qapplication called gui, since it's the gui frontend for the program
-
-gui = QApplication(sys.argv) #sys.argv allows for cli switches
-
-#qwidget acts as the windo
-window = QWidget()
-
-#have to show the window since all top level widgets are hidden
-window.show()
-
-#now start the event loop
-gui.exec()
-'''
 from tkinter import * 
 
 from tkinter.filedialog import askopenfilename #filepicker
