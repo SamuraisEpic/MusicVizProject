035bd83832a217a8f10d0ad8eae22363d93226c2
SamuraisEpic <SamuraisEpic@hotmail.com>
2023-06-17 22:05:27 -0400

    Update design.py

--- design.py
+++ design.py
@@ -1,11 +1,3 @@
 '''
-i don't know how design will work, but we'll give this thing a shot. have to reset because my idiocy
-
-<proposal goes here>
-various expanding points
-
-for one, qt is picked as it is the preferred gui framework for KDE apps, and on a plasma based system (or multiple)  i thought that to be the most logical choice.
-
-waveform will be provided through a library, and will be transhformed into an animation somehow. that part is to still be determined, but i have a few ideas in mind, that in clude half height bars and using those to make a small animation.
-
+this will be made soon
 '''
