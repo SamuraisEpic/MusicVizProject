4fd15e20f29d8b8268a90579ed52bb47a0963090
SamuraisEpic <69528792+SamuraisEpic@users.noreply.github.com>
2023-06-21 07:26:14 -0400

    new file:   WindowsNotifySystemGeneric.wav - test sound 	modified:   main.py - proper wave filter

--- main.py
+++ main.py
@@ -4,9 +4,11 @@
 #start by importing sys for cli switches, this would help if the user wants to interact through a terminal similar to qemu.
 #also relevant modules from pyqt5 (obviously)
 
-from tkinter import * 
+from tkinter import * #gui
 
 from tkinter.filedialog import askopenfilename #filepicker
+
+import wave
 
 #make a window 
 mainwindow = Tk()
@@ -24,8 +26,17 @@ txt.grid()
 #button and stuff
     #file picker function
 def generate():
-    filename = askopenfilename()
+    filename = askopenfilename(filetypes=(("Wave files", "*.wav"),))
+    print(filename)
     #okie now that we have a path we can start doing fun stuff
+    soundin = wave.open("" + filename + "",'rb')
+    samplerate = soundin.getframerate()
+    totalsamples = soundin.getnframes()
+    totalduration = totalsamples/samplerate
+    totalchannels = soundin.getnchannels()
+    
+
+
 
 btn = Button(mainwindow, text = "generate!", command = generate) #button to open file picker/choose audiofile
 
